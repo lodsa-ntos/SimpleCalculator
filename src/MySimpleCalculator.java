da74ce96c8ba183fbec777e5ebd463c646a0bcd0
Lodney Santos <a21505293@alunos.ulht.pt>
2023-10-19 23:36:01 +0100

    invoke the calculator window

--- src/MySimpleCalculator.java
+++ src/MySimpleCalculator.java
@@ -5,8 +5,12 @@ public class MySimpleCalculator extends JFrame {
 
     public static void main(String[] args) {
 
+        // Create the Calculator Window
+        // SwingUtilities - A collection of utility methods for Swing
+        javax.swing.SwingUtilities.invokeLater(() -> {
 
-        
+        });
+
     }
 
 }
